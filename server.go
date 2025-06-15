da1fe7c89cfb79c5284ae1aaf2a5c25a366dce89
Gianni Weinand <ggweinand@gmail.com>
2025-06-15 18:35:09 +0200

    Separation between task and landscape tiles

--- server.go
+++ server.go
@@ -8,21 +8,29 @@ import (
 	"os"
 )
 
-func getTiles(w http.ResponseWriter, r *http.Request, tiles []Tile) {
+func getTiles(w http.ResponseWriter, r *http.Request, landscape []Tile, task []Tile) {
 	query := r.PathValue("query")
 	regex := QueryToRegexp(query)
 	fmt.Fprintf(w, "Processing query: %s\n", query)
-	fmt.Fprintf(w, "The regex is: %v\n", regex)
+	//fmt.Fprintf(w, "The regex is: %v\n", regex)
 
-	var matches int
-	for _, tile := range tiles {
-
+	var lMatches, tMatches int
+	for _, tile := range landscape {
 		if tile.Matches(regex){
 			fmt.Fprintf(w, "%v\n", tile)
-			matches++
+			lMatches++
 		}
 	}
-	fmt.Fprintf(w, "Total amount of matches: %v\n", matches)
+	fmt.Fprintf(w, "Landscape matches: %v\n", lMatches)
+
+	for _, tile := range task {
+		if tile.Matches(regex){
+			fmt.Fprintf(w, "%v\n", tile)
+			tMatches++
+		}
+	}
+	fmt.Fprintf(w, "Task matches: %v\n", tMatches)
+	fmt.Fprintf(w, "Total matches: %v\n", tMatches + lMatches)
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -33,11 +41,20 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, string(message))
 }
 
+func initTiles(filenames []string) []Tile {
+	var tiles []Tile
+	for _, filename := range filenames {
+		loaded, _ := LoadTilesFromFile("tiles/" + filename + ".txt")
+		tiles = append(tiles, loaded...)
+	}
+	return tiles
+}
 
 func main() {
-	taskTiles, _ := LoadTilesFromFile("tiles/task_tiles.txt")
-	taskTilesHandler := func(w http.ResponseWriter, r *http.Request) { getTiles(w, r, taskTiles) }
-	http.HandleFunc("GET /tiles/{query}", taskTilesHandler)
+	landscape := initTiles([]string {"landscape", "box_2"})
+	task := initTiles([]string{"task", "box_1"})
+	getTilesHandler := func(w http.ResponseWriter, r *http.Request) { getTiles(w, r, landscape, task) }
+	http.HandleFunc("GET /tiles/{query}", getTilesHandler)
 
 	http.HandleFunc("GET /", homeHandler)
 
